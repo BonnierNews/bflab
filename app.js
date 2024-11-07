7ba5c6300ff7e092be8886dde3858bef6f7b695d
Per-Erik Ferb <per-erik.ferb@bonniernews.se>
2024-11-07 22:36:19 +0100

    Arithmetic, movement, echo and one loop ✅

--- app.js
+++ app.js
@@ -10,6 +10,57 @@ import { readFileSync } from "fs";
  * @returns {Promise<void>}
  */
 export async function run(program, input, output) {
+  const memory = new Uint8Array(30000);
+  let pointer = 0;
+
+  const data = [];
+  for await (const chunk of input) {
+    data.push(chunk);
+  }
+  const inputStr = Buffer.concat(data).toString();
+  let inputIndex = 0;
+
+  for (let instructionIndex = 0; instructionIndex < program.length; instructionIndex++) {
+    switch (program[instructionIndex]) {
+      case ".":
+        output.write(String.fromCharCode(memory[pointer]));
+        break;
+      case "+":
+        memory[pointer]++;
+        break;
+      case "-":
+        memory[pointer]--;
+        break;
+      case ",":
+        memory[pointer] = inputStr.charCodeAt(inputIndex);
+        inputIndex++;
+        break;
+      case ">":
+        pointer++;
+        break;
+      case "<":
+        pointer--;
+        break;
+      case "[":
+        if (memory[pointer] === 0) {
+          let i = instructionIndex;
+          while (program[instructionIndex] !== "]") {
+            i++;
+          }
+          instructionIndex = i--; // subtract 1 because the loop will increment it
+        }
+        break;
+      case "]":
+        if (memory[pointer] !== 0) {
+          let j = instructionIndex;
+          while (program[j] !== "[") {
+            j--;
+          }
+          instructionIndex = j--; // subtract 1 because the loop will increment it
+        }
+        break;
+    }
+  }
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
--- test/feature/hello-world.js
+++ test/feature/hello-world.js
@@ -110,8 +110,7 @@ Feature("Hello World", () => {
     When("Running the hello world program with a loop", async () => {
       const program = `
 +++++ +++++             initialize counter (cell #0) to 10
-[                       use loop to set the next four cells to
-70/100/30/10
+[                       use loop to set the next four cells to 70/100/30/10
 > +++++ ++              add  7 to cell #1
 > +++++ +++++           add 10 to cell #2
 > +++                   add  3 to cell #3
