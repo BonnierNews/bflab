00bf086da86dd17ba176729cd53fc4d3eea6c2ac
Sofia Fahlesson <sofia.fahlesson@bonniernews.se>
2024-11-07 15:38:38 +0100

    Testeroni
    Co-authored-by: Oscar Mattsson <liroliro@users.noreply.github.com>
    Co-authored-by: Sanna Rossäng <sannarossang@users.noreply.github.com>

--- app.js
+++ app.js
@@ -8,8 +8,64 @@ import { readFileSync } from "fs";
  * @param {import("stream").Readable} input
  * @param {import("stream").Writable} output
  * @returns {Promise<void>}
- */
+*/
 export async function run(program, input, output) {
+  const arr = [ ...program ];
+
+  const size = 3000;
+  const memory = new Uint8Array(size);
+  let pointer = 0;
+
+  for (const char of arr) {
+    switch (char) {
+      case ".":
+        await output.write(intToUnicode(memory[pointer] || 0));
+        break;
+      case "+":
+        memory[pointer] = Number(memory[pointer] || 0) + 1;
+        break;
+      case "-":
+        memory[pointer] = Number(memory[pointer] || 0) - 1;
+        break;
+      case ">":
+        if (pointer === size - 1) {
+          pointer = 0;
+          break;
+        }
+        pointer += 1;
+        break;
+      case "<":
+        if (pointer === 0) {
+          pointer = size - 1;
+          break;
+        }
+        pointer -= 1;
+        break;
+
+      case "[":
+        if (!memory[pointer]) {
+          let openLoops = 1;
+          while (openLoops) {
+            const instruction = arr.shift();
+            if (instruction === "[") {
+              openLoops += 1;
+            } else if (instruction === "]") {
+              openLoops -= 1;
+            }
+          }
+        }
+        break;
+    }
+  }
+}
+
+/**
+ * Converts an integer to a Unicode string.
+ * @param {number} num - The integer number to convert.
+ * @returns {string} The corresponding Unicode character.
+ */
+function intToUnicode(num) {
+  return String.fromCharCode(num);
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
