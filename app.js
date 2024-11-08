82477bd4ff4a0f8bc0f3a820d5e34992d59ce095
Peter Pettersson <peter.pettersson@bonniernews.se>
2024-11-08 12:00:39 +0100

    Small rewrites

--- app.js
+++ app.js
@@ -7,61 +7,68 @@ import { readFileSync } from "fs";
  * @param {string} program
  * @param {import("stream").Readable} input
  * @param {import("stream").Writable} output
- * @returns {void}
+ * @returns {Promise<void>}
  */
-export function run(program, input, output) {
-  const cells = [ "\u0000" ];
-  let currentCell = 0;
+export async function run(program, input, output) {
+  const cells = new Uint8Array(1000);
+  let cellPointer = 0;
+
+  const chunks = [];
+  for await (const chunk of input) {
+    chunks.push(chunk);
+  }
+  let unprocessedInput = Buffer.concat(chunks).toString();
 
   for (let i = 0; i < program.length; i++) {
-    if (program[i] === "+") {
-      cells[currentCell] = String.fromCharCode((cells[currentCell].charCodeAt(0) + 1) % 256);
-    } else if (program[i] === "-") {
-      cells[currentCell] = String.fromCharCode((256 + cells[currentCell].charCodeAt(0) - 1) % 256);
-    } else if (program[i] === ".") {
-      output.write(cells[currentCell]);
-    } else if (program[i] === ",") {
-      const char = input.read(1)?.toString();
-      cells[currentCell] = char;
-    } else if (program[i] === ">") {
-      currentCell++;
-
-      if (currentCell === cells.length) {
-        cells.push("\u0000");
-      }
-    } else if (program[i] === "<") {
-      if (currentCell === 0) {
-        throw new Error("Cannot move to a negative cell");
-      }
-
-      currentCell--;
-    } else if (program[i] === "[") {
-      if (cells[currentCell] !== "\u0000") {
-        continue;
-      }
-
-      let bracketCount = 1;
-      while (bracketCount !== 0) {
-        i++;
-        if (program[i] === "[") {
-          bracketCount++;
-        } else if (program[i] === "]") {
-          bracketCount--;
+    switch (program[i]) {
+      case "+":
+        cells[cellPointer]++;
+        break;
+      case "-":
+        cells[cellPointer]--;
+        break;
+      case ".":
+        output.write(String.fromCharCode(cells[cellPointer]));
+        break;
+      case ",":
+        cells[cellPointer] = unprocessedInput.charCodeAt(0);
+        unprocessedInput = unprocessedInput.slice(1);
+        break;
+      case ">":
+        cellPointer++;
+        break;
+      case "<":
+        cellPointer--;
+        break;
+      case "[": {
+        if (cells[cellPointer] !== 0) {
+          continue;
         }
-      }
-    } else if (program[i] === "]") {
-      if (cells[currentCell] === "\u0000") {
-        continue;
-      }
-
-      let bracketCount = 1;
-      while (bracketCount !== 0) {
-        i--;
-        if (program[i] === "[") {
-          bracketCount--;
-        } else if (program[i] === "]") {
-          bracketCount++;
+        let bracketCount = 1;
+        while (bracketCount !== 0) {
+          i++;
+          if (program[i] === "[") {
+            bracketCount++;
+          } else if (program[i] === "]") {
+            bracketCount--;
+          }
         }
+        break;
+      }
+      case "]": {
+        if (cells[cellPointer] === 0) {
+          continue;
+        }
+        let bracketCountBack = 1;
+        while (bracketCountBack !== 0) {
+          i--;
+          if (program[i] === "[") {
+            bracketCountBack--;
+          } else if (program[i] === "]") {
+            bracketCountBack++;
+          }
+        }
+        break;
       }
     }
   }
@@ -75,7 +82,7 @@ if (process.argv[1] === fileURLToPath(import.meta.url)) {
   }
 
   try {
-    run(readFileSync(process.argv[2]).toString(), process.stdin, process.stdout);
+    await run(readFileSync(process.argv[2]).toString(), process.stdin, process.stdout);
     process.exit(0);
   } catch (e) {
     if (e.code === "ENOENT") {
